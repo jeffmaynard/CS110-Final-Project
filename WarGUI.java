beea184bebb8cd4079730051cceb086bae04f679
Jeff Maynard <jpmaynar@uvm.edu>
2014-04-30 14:17:33 -0400

    GUI doesn't function - null pointer on button

--- WarGUI.java
+++ WarGUI.java
@@ -1,6 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.io.*;
 /**
 Jeff Maynard
 CS110
@@ -9,34 +10,47 @@ Card Game of War - GUI
 */
 
 public class WarGUI extends JFrame{
-   private JPanel leftPanel, rightPanel, bottomPanel;
-   private JLabel label;
+   private War game;
+   private JPanel leftPanel, rightPanel;
+   private JButton button;
 
    public WarGUI(String title){
       super(title);
-      setSize(500, 500);
+      game = new War();
+      setSize(700, 500);
       setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      setLayout(new GridLayout(1,2));
       
-      setLayout(new GridLayout(2,2));
+      //button
+      button = new JButton("ADVANCE");
+      button.addActionListener(new ButtonListener());
       //left panel
       leftPanel = new JPanel();
       leftPanel.setSize(new Dimension(250, 200));
       leftPanel.setBackground(Color.black);
+      leftPanel.add(new JLabel(new ImageIcon("cardPics/back.jpg")));
       //right panel
       rightPanel = new JPanel();
       rightPanel.setSize(new Dimension(250, 200));
       rightPanel.setBackground(Color.blue);
-      //bottom panel
-      bottomPanel = new JPanel();
-      bottomPanel.setSize(new Dimension(500, 100));
-      bottomPanel.setBackground(Color.green);
+      rightPanel.add(new JLabel(new ImageIcon("cardPics/back.jpg")));
       //add
       add(leftPanel, BorderLayout.NORTH);
       add(rightPanel, BorderLayout.NORTH);
-      add(bottomPanel, BorderLayout.SOUTH);
+      add(button, BorderLayout.EAST);
+      
+   }
+   private class ButtonListener implements ActionListener{
+      public void actionPerformed(ActionEvent e){ 
+         button = (JButton)(e.getSource());
+         // make a move
+         if(game.player1.deal().getClass().getName().equals("Card")){
+            rightPanel.add(new JLabel(new ImageIcon("cardPics/back.jpg"))); 
+         }
+      }
    }
    public static void main(String[] args){
-      WarGUI frame = new WarGUI("WAR!");
+      WarGUI frame = new WarGUI("WAR! by Jeff Maynard");
       frame.setLocationRelativeTo(null);
       frame.setVisible(true);
    }
